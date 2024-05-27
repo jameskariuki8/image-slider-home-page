a9fa1399522afc76c0a0b7f0b969ff024132e9fc
jameskariuki8 <juniorkariuki735@gmail.com>
2024-05-27 21:34:13 +0300

    responsive home page

---
+++ js/categories-scripts-917a9b53.js
@@ -0,0 +1,2 @@
+import"./teaser-stats-99d808c6.js";
+//# sourceMappingURL=categories-scripts-917a9b53.js.map
---
+++ js/shots-scripts-8562484d.js
@@ -0,0 +1,2 @@
+import"./large-shot-view-recorder-b4135944.js";
+//# sourceMappingURL=shots-scripts-8562484d.js.map
